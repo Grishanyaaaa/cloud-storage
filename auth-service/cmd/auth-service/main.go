423f8cf51c00cea76ad166887dcab540c2a40066
grigoriy <dinsuil275283@gmail.com>
2026-04-24 00:26:46 +0500

    rename imports

--- auth-service/cmd/auth-service/main.go
+++ auth-service/cmd/auth-service/main.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"github.com/Grishanyaaaa/cloud-storage/auth-service/internal/infrastructure/config"
-	"github.com/Grishanyaaaa/cloud-storage/auth-service/pkg/logger"
+	"github.com/Grishanyaaaa/cloud-storage/auth-service/pkg/common/logger"
 )
 
 func main() {
