ab7feca2118c0a40533e723c5096e7f5f60b7d46
grigoriy <dinsuil275283@gmail.com>
2026-04-24 01:28:38 +0500

    create security config

---
+++ auth-service/internal/infrastructure/config/security_config.go
@@ -0,0 +1,7 @@
+package config
+
+type SecurityConfig struct {
+	BcryptCost        int `env:"SECURITY_BCRYPT_COST" env-default:"12"`
+	MinPasswordLength int `env:"SECURITY_MIN_PASSWORD_LENGTH" env-default:"8"`
+	MaxPasswordLength int `env:"SECURITY_MAX_PASSWORD_LENGTH" env-default:"72"`
+}
