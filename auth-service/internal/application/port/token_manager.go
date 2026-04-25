1aca07f8d890c24b1ef0ef0ab37f730a004b9756
grigoriy <dinsuil275283@gmail.com>
2026-04-25 19:31:01 +0500

    create token manager contract

---
+++ auth-service/internal/application/port/token_manager.go
@@ -0,0 +1,21 @@
+package port
+
+import "time"
+
+type TokenClaims struct {
+	UserID string
+	Email  string
+}
+
+type TokenPair struct {
+	AccessToken  string
+	RefreshToken string // сырой токен, хешировать перед записью в базу
+}
+
+type TokenManager interface {
+	GenerateAccessToken(claims TokenClaims) (string, error)
+	GenerateRefreshToken() (string, error)
+	ParseAccessToken(token string) (*TokenClaims, error)
+	AccessTokenTTL() time.Duration
+	RefreshTokenTTL() time.Duration
+}
