8dd760cc5334a9841d09213d4a261939cc63fe8c
grigoriy <dinsuil275283@gmail.com>
2026-04-30 23:27:26 +0500

    feat(gateway): add structured logging support
    - Setup logger with environment-based configuration
    - Pretty logging for LOCAL env
    - JSON logging for DEV/PROD
    - Discard handler for tests

---
+++ api-gateway/pkg/common/logger/logger.go
@@ -0,0 +1,56 @@
+package logger
+
+import (
+	"log/slog"
+	"os"
+
+	"github.com/Grishanyaaaa/cloud-storage/api-gateway/pkg/common/logger/handlers/slogdiscard"
+	"github.com/Grishanyaaaa/cloud-storage/api-gateway/pkg/common/logger/handlers/slogpretty"
+)
+
+const (
+	envLocal = "LOCAL"
+	envDev   = "DEV"
+	envProd  = "PROD"
+)
+
+// SetupLogger initializes the logger based on the environment.
+func SetupLogger(env string) *slog.Logger {
+	var log *slog.Logger
+
+	switch env {
+	case envLocal:
+		log = setupPrettySlog()
+	case envDev:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+		)
+	case envProd:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}),
+		)
+	default: // If env config is invalid, set prod settings by default due to security
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}),
+		)
+	}
+
+	return log
+}
+
+func setupPrettySlog() *slog.Logger {
+	opts := slogpretty.PrettyHandlerOptions{
+		SlogOpts: &slog.HandlerOptions{
+			Level: slog.LevelDebug,
+		},
+	}
+
+	handler := opts.NewPrettyHandler(os.Stdout)
+
+	return slog.New(handler)
+}
+
+// SetupDiscardLogger creates a logger that discards all output (for tests).
+func SetupDiscardLogger() *slog.Logger {
+	return slog.New(slogdiscard.NewDiscardHandler())
+}
